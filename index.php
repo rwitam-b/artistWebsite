e7203c15cbde569815b58b12f24b23bb854d5814
Rwitam Bandyopadhyay <mailtorwitam@gmail.com>
2017-03-12 22:31:11 +0530

    Update index.php

--- index.php
+++ index.php
@@ -40,7 +40,7 @@
         </div>
     </section>
     <section id="work">
-      <iframe id="soundcloudFrame" class="wow lightSpeedIn" data-wow-delay="2s" width="100%" height="450" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/users/31383641&amp;color=ff5500&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=true"></iframe>
+      <iframe id="soundcloudFrame" class="wow fadeInLeft" data-wow-delay="2s" width="100%" height="450" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/users/31383641&amp;color=ff5500&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=true"></iframe>
     </section>
     <div style="height:4000px;background-color:gainsboro;"></div>
 </body>
