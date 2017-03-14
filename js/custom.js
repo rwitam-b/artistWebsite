7ad48894ea369d8961cdad4caa142da368ec9a56
Rwitam Bandyopadhyay <mailtorwitam@gmail.com>
2017-03-14 23:52:51 +0530

    Add files via upload

--- js/custom.js
+++ js/custom.js
@@ -1,10 +1,9 @@
-flag = false;
 $(window).scroll(function() {
     var val = $(this).scrollTop();
     $(".greet").css({
         'transform': 'translate(0px,' + val / 4 + '%)',
-        '-webkit-filter': 'drop-shadow(5px 5px 5px hsl(0, 100%, ' + val / 10 + '%))',
-        'filter': 'drop-shadow(5px 5px 5px hsl(0, 100%, ' + val / 10 + '%))'
+        '-webkit-filter': 'drop-shadow(5px 5px 5px hsl(0, 100%, ' + val / 5 + '%))',
+        'filter': 'drop-shadow(5px 5px 5px hsl(0, 100%, ' + val / 5 + '%))'
     });
     $("#soundcloudFrame").one('animationend', function() {
         var widget = SC.Widget("soundcloudFrame");
