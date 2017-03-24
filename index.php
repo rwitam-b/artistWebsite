8092c0e652ca6380db1d868cf84bfffb61604ead
Rwitam Bandyopadhyay <mailtorwitam@gmail.com>
2017-03-24 11:44:16 +0530

    Version 2
    Navbar changed
    Story Section fades added
    Social Media Section started

--- index.php
+++ index.php
@@ -7,41 +7,81 @@
     <link rel="stylesheet" href="css/animate.css">
     <link rel="stylesheet" href="css/app.css">
     <script src="js/jquery.js"></script>
-    <script src="js/wow.js"></script>
     <script src="bootstrap-3.3.7/js/bootstrap.min.js"></script>
-    <script src="https://w.soundcloud.com/player/api.js"></script>
     <script src="js/custom.js"></script>
     <script>
-        new WOW().init();
+        $(document).ready(function() {
+            setup();
+        });
     </script>
 </head>
 
-<body>
-    <header>
+<body data-spy="scroll" data-target=".navbar" data-offset="50">
+    <nav style="display:none;" class="navbar navbar-default navbar-fixed-top">
+        <div class="container">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+                  <span class="icon-bar"></span>
+                  <span class="icon-bar"></span>
+                  <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="#home"><strong>Mitch Mula Beats</strong></a>
+            </div>
+            <div class="collapse navbar-collapse" id="myNavbar">
+                <ul class="nav navbar-nav navbar-right text-center">
+                    <li><a href="#story">My Story</a></li>
+                    <li><a href="#social">Social Media</a></li>
+                    <li><a href="#">Page 3</a></li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <header id="home">
         <div class="greet">
             <img class="animated zoomInDown" src="images/logo-white.png">
         </div>
     </header>
     <section id="story">
-        <div class="row vertical-align wow fadeIn" data-wow-duration="5s">
-            <div class="col-sm-12 col-lg-6 text-center">
+        <div class="row vertical-align">
+            <div class="col-xs-12 col-lg-6 text-center">
                 <br><br>
-                <h1 class="heading animated infinite pulse">My Story</h1>
                 <div class="container-fluid text">
+                  <hr><h1 class="lightHeading text-center">My Story</h1><hr>
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum eget molestie nulla, eu volutpat velit. Donec aliquet est aliquet nibh vehicula mattis. Sed ac sodales risus, et mattis tellus. Fusce faucibus dui eget augue bibendum semper. Maecenas
                     ut porttitor lacus. Integer rutrum feugiat ante mattis finibus. Phasellus et leo eget risus commodo congue. Donec sed ultrices nisl. Suspendisse posuere, odio in maximus ultricies, lacus turpis eleifend odio, sed viverra nibh orci
                     et ex. Sed ut sem eu erat imperdiet posuere id vel enim. Nullam risus purus, consequat ut fringilla id, vestibulum mollis elit. Nullam a lacus posuere, tincidunt urna in, iaculis lacus. Sed a tempus arcu, a tempus mi. Nam et mattis
                     arcu. Pellentesque mattis iaculis molestie.
                 </div>
             </div>
-            <div class="col-sm-12 col-lg-6 text-center">
+            <div class="col-xs-12 col-lg-6 text-center">
                 <img class="img-responsive" src="images/me.jpeg">
             </div>
         </div>
     </section>
-    <!-- <section id="work">
-        <iframe id="soundcloudFrame" class="wow fadeIn" data-wow-delay="2s" width="100%" height="450" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/users/31383641&amp;color=ff5500&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=true"></iframe>
-    </section> -->
+    <section id="social">
+      <div class="container text-center"><br><br>
+        <hr><h1 class="darkHeading">Social Media</h1><hr>
+        <div class="row">
+          <div class="col-xs-6">
+            <img src="images/youtube.png">
+          </div>
+          <div class="col-xs-6">
+            <img src="images/instagram.png">
+          </div>
+        </div><br><br>
+        <div class="row">
+          <div class="col-xs-6">
+            <img src="images/facebook.png">
+          </div>
+          <div class="col-xs-6">
+            <img src="images/twitter.png">
+          </div>
+          <div class="col-xs-12">
+            <img src="images/soundcloud.png">
+          </div>
+        </div>
+      </div>
+    </section>
     <div style="height:4000px;background-color:gainsboro;"></div>
 </body>
 
--- js/custom.js
+++ js/custom.js
@@ -1,12 +1,35 @@
+// Navbar Link Clik Smooth Scroll
+function setup() {
+    $("nav a").on('click', function(event) {
+        if (this.hash !== "") {
+            event.preventDefault();
+            var hash = this.hash;
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 800, function() {
+                window.location.hash = hash;
+            });
+        }
+    });
+}
+
 $(window).scroll(function() {
     var val = $(this).scrollTop();
+
+    // Navbar Fade
+    if ($(window).scrollTop() > 0.5 * $(window).height()) {
+        $("nav").fadeIn("slow");
+    } else {
+        $("nav").fadeOut("slow");
+    }
+
+    // MitchMula Logo Parallax
     $(".greet").css({
         'transform': 'translate(0px,' + val / 4 + '%)',
         '-webkit-filter': 'drop-shadow(5px 5px 5px hsl(0, 100%, ' + val / 5 + '%))',
         'filter': 'drop-shadow(5px 5px 5px hsl(0, 100%, ' + val / 5 + '%))'
     });
-    $("#soundcloudFrame").one('animationend', function() {
-        var widget = SC.Widget("soundcloudFrame");
-        widget.play();
-    });
+
+    // Story Section Fading
+    $("#story div").css("opacity",Math.round((parseFloat(val)/(0.8*$("#story").offset().top))*100)/100);
 });
